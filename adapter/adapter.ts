55090be4942ab3f01b56826d6bac88b0e4f3b869
Juhani Ränkimies <juhani@juranki.com>
2021-04-05 20:51:04 +0000

    copy svelte output

--- adapter/adapter.ts
+++ adapter/adapter.ts
@@ -1,9 +1,30 @@
 import type { Adapter } from '@sveltejs/kit'
+import * as path from 'path'
+import * as fs from 'fs'
+
+const rmRecursive = (p:string) => {
+    if (!fs.existsSync(p)) return
+    const stats = fs.statSync(p)
+    if (stats.isDirectory()) {
+        fs.readdirSync(p).forEach(f => {
+            rmRecursive(path.join(p, f))
+        })
+        fs.rmdirSync(p)
+    } else {
+        fs.unlinkSync(p)
+    }
+}
 
 export const adapter: Adapter = {
     name: 'MAGIC',
     async adapt(utils): Promise<void> {
-        console.log('TODO...')
+        const contentPath = path.join(__dirname, 'content')
+        rmRecursive(contentPath)
+        const serverPath = path.join(contentPath, 'server')
+        const staticPath = path.join(contentPath, 'static')
+        utils.copy_server_files(serverPath)
+        utils.copy_client_files(staticPath)
+        utils.copy_static_files(staticPath)
     }
 }
 
